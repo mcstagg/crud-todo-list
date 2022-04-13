a0f75e1fc8e3bf327f5c917e8c42c34c3a4a8d48
Mike Stagg <mikecstagg@gmail.com>
2022-04-12 23:45:02 -0700

    inital commit... add & remove working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = e => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput(['']);
+  }
+
+  const removeTodo = ({ e, currentToDo}) => {
+    const removeItem = todos.filter((todo) => todo !== currentToDo);
+    setTodos(removeItem);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Welcome to My TODO List</h1>
+      <form> 
+        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" />
+        <button type="submit" onClick={addTodo}>Add TODO</button>
+      </form>
+
+      <h2>List of TODOs:</h2>
+      {todos.map((todo, i) => (
+        <li key={i}>
+          {todo}
+          <button onClick={(e) => removeTodo({ e: e, currentToDo: todo})}>Delete</button>
+        </li>
+      ))}  
     </div>
-  );
+  )
 }
 
 export default App;
