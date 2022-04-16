2236c5139be7bc58a83a3e49f04dd3178404e3d9
Mike Stagg <mikecstagg@gmail.com>
2022-04-16 15:57:01 -0700

    main functionality completed and TODOs marked all throughout app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import './App.css';
 
 // Your client requires a TODO list application, they have submitted a list of requirements. The application must have the following features:
 
+// TODO: The TODO app should be styled appropriately (you could use Glassmorphism!)
+
+// TODO: At the top of the page, the client wants an inspirational quote which is different each time the user refreshes the page. The quote should be fetched from the following API: https://type.fit/api/quotes
+
+// TODO: Refactor the app and break it up into components
+
+// TODO: CHALLENGE 1: Persist the TODO's so that when the user refreshes the page, the list of TODO's is preserved!
+
+// TODO: CHALLENGE 2: Add animations to the TODO list using this library!
+
+// TODO: The client requests that the code is submitted to Github for review, once you have done this, fill this form to send the assignment to the client!
+
 const App = () => {
   const create = useRef(null);
   // TODO: fix update button click input focus 
@@ -13,14 +25,26 @@ const App = () => {
   const [input, setInput] = useState('');
   const [updateInput, setUpdateInput] = useState('');
   const [updateClicked, setUpdateClicked] = useState(false);
-  const [selectedTask, setSelectedTask] = useState(0);
+  //const [beingEdited, setBeingEdited] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(undefined);
 
-  // Focus on the CREATE input field on mount
+  // Focus on the CREATE input field on mount/inital render only
   useEffect(() => {
+    console.log(create);
+    console.log(create.current);
     create.current.focus();
   }, []);
 
+  // // Listen for update button click event and focus the curosr in the update input field
+  // useEffect(() => {
+  //   const pointer = update.current;
+  //   return () => {
+  //     pointer.focus();
+  //   }
+  // }, [update])
+
   // CREATE - The ability to type into an input field and hit the enter key to add a TODO to a list of TODO's
+  // TODO: Auto Capitalize the first letter of each todo
   const addTodo = e => {
     e.preventDefault();
     setTodos([...todos, input]);
@@ -31,16 +55,21 @@ const App = () => {
 
   // UPDATE - The ability to update a todo from the todo list
   const updateTodo = i => {
+    // remove current todo item from dom
+    //setBeingEdited(true);
     // render edit input field & button onclick of update
     setUpdateClicked(true);
     // but only for the specifically selected task
     setSelectedTask(i);
     // gather input in JSX
-    // focus on the input field
+    console.log(update);
+    console.log(update.current);
     update.current.focus();
   }
 
   const confirmEdit = (task, i) => {
+    // add edited todo back to the dom
+    //setBeingEdited(false);
     // update the todo onclick or onsumbit
     const newTodos = [...todos];
     //newTodos.push(task);
@@ -66,16 +95,20 @@ const App = () => {
 
       {/* READ */}
       <h2>READ TODOs:</h2>
+      {/* TODO: I'm not sure using the index as the key is best practice. What else? */}
       {todos.map((task, i) => (
         <li key={i}>
+          {/* TODO: Replace task with input field with ph text on update click */}
           {task}
           {updateClicked === true && selectedTask === i && (
             <>
+              {/* TODO: Add enter button submit functionality */}
               <input ref={update} value={updateInput} onChange={e => setUpdateInput(e.target.value)} type="text"/>
-              <button onClick={() => confirmEdit(task, i)}>Confirm Edit</button> 
+              <button onClick={() => confirmEdit(task, i)}>CONFIRM EDIT</button> 
             </>
           )}
           <button onClick={() => updateTodo(i)}>UPDATE</button>
+          {/* TODO: Add button on each TODO to indicate completion of a TODO, the text should turn green and have a strikethrough once completed */}
           <button onClick={() => removeTodo(i)}>DELETE</button>
         </li>
       ))}  
